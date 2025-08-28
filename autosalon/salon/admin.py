42afaf6bcb3e0af0d07769b1d6165d27f5cbe145
Muhammad <muhammadrasulaxmedov666@gmail.com>
2025-08-28 10:55:53 +0500

    Admin paneldan kirganda, filter bor,mashina yoli va brandi boyicha

--- autosalon/salon/admin.py
+++ autosalon/salon/admin.py
@@ -1,15 +1,17 @@
 from django.contrib import admin
-
 from .models import Brand, Car
+
 
 @admin.register(Brand)
 class BrandAdmin(admin.ModelAdmin):
     list_display = ("id", "name", "country")
     search_fields = ("name", "country")
 
+
 @admin.register(Car)
 class CarAdmin(admin.ModelAdmin):
     list_display = ("id", "name", "brand", "year", "price")
     list_filter = ("brand", "year")
     search_fields = ("name",)
+    ordering = ("-year",)
 
